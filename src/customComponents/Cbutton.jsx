bd0a860275e79c450125916a7914bf6aee0462a0
rayen <ray682212@gmail.com>
2024-10-22 21:00:13 +0100

    colors added

---
+++ src/customComponents/Cbutton.jsx
@@ -0,0 +1,7 @@
+/* eslint-disable react/prop-types */
+import React from "react";
+import { colors } from "../constants/colors";
+
+export default function Cbutton({ children }) {
+  return <button>{children}</button>;
+}
---
+++ src/customComponents/Cp.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Cp({ children }) {
+  return <p>{children}</p>;
+}
