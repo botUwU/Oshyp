a0eb8f63fbc2755359e77eb07a0efefc9e209592
rayen <ray682212@gmail.com>
2024-10-22 22:17:19 +0100

    edit global styles

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -1,17 +1,38 @@
 // src/GlobalStyle.js
 import { createGlobalStyle } from "styled-components";
-import MyCustomFont from "./assets/fonts/PlaywriteGBS-ExtraLight.ttf"; // Adjust the path accordingly
+import ExtraLight from "./assets/fonts/PlaywriteGBS-ExtraLight.ttf";
+import ExtraLightItalic from "./assets/fonts/PlaywriteGBS-ExtraLightItalic.ttf";
+import Italic from "./assets/fonts/PlaywriteGBS-Italic.ttf";
+import Light from "./assets/fonts/PlaywriteGBS-Light.ttf";
+import LightItalic from "./assets/fonts/PlaywriteGBS-LightItalic.ttf";
+import Regular from "./assets/fonts/PlaywriteGBS-Regular.ttf";
+import Thin from "./assets/fonts/PlaywriteGBS-Thin.ttf";
+import ThinItalic from "./assets/fonts/PlaywriteGBS-ThinItalic.ttf";
+
+// Adjust the path accordingly
 
 const GlobalStyle = createGlobalStyle`
-  @font-face {
-    font-family: 'MyCustomFont';
-    src: url(${MyCustomFont}) format('woff2');
+@font-face {
+    font-family: 'PlaywriteGBS-Regular';
+    src: url(${Regular}) format('truetype');
+    font-weight: normal;
+    font-style: normal;
+  }
+@font-face {
+    font-family: 'PlaywriteGBS-Italic';
+    src: url(${Italic}) format('truetype');
     font-weight: normal;
     font-style: normal;
   }
 
+  @font-face {
+    font-family: 'PlaywriteGBS-ExtraLight';
+    src: url(${ExtraLight}) format('truetype');
+    font-weight: 200; // Extra Light weight
+    font-style: normal;
+  }
+
   body {
-    font-family: 'MyCustomFont', sans-serif; /* Default font */
     margin: 0;
     padding: 0;
     box-sizing: border-box;
